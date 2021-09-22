29e477d08ace494f53a039863b55ccbce3b10d28
Romuald-Big-Screen <romuald.torro@gmail.com>
2021-09-22 15:48:19 +0200

    TransferController

---
+++ src/main/java/com/PayMyBudy/service/TransferService.java
@@ -0,0 +1,42 @@
+package com.PayMyBudy.service;
+
+import com.PayMyBudy.model.Account;
+import com.PayMyBudy.model.Transfer;
+import com.PayMyBudy.model.User;
+import com.PayMyBudy.repository.AccountRepository;
+import com.PayMyBudy.repository.UserRepository;
+
+
+import java.security.Security;
+import java.util.List;
+
+
+public class TransferService {
+
+    private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
+
+    public TransferService(UserRepository userRepository, AccountRepository accountRepository) {
+        this.userRepository = userRepository;
+        this.accountRepository = accountRepository;
+    }
+
+    public List<Transfer>findTransaction() {
+        org.springframework.security.core.userdetails.User springUser = (org.springframework.security.core.userdetails.User) Security;
+        User connectedUser = userRepository.findUserByMail(springUser.getUsername())
+                .orElseThrow(() -> new RuntimeException("user with email not found"));
+        return transferRepository.findTransferByUserId(connectedUser.getId());
+
+    }
+
+    public String findIban() {
+        org.springframework.security.core.userdetails.User springUser = (org.springframework.security.core.userdetails.User) Security;
+        User connectedUser = userRepository.findUserByMail(springUser.getUsername())
+                .orElseThrow(() -> new RuntimeException("user with email not found"));
+        Account account = accountRepository.findAccountByUserId(connectedUser.getId());
+        return account.getIban();
+
+
+    }
+
+}
---
+++ src/main/java/com/PayMyBudy/webApp/controller/TransferController.java
@@ -0,0 +1,74 @@
+package com.PayMyBudy.webApp.controller;
+
+
+
+import com.PayMyBudy.model.Transfer;
+import com.PayMyBudy.model.User;
+import com.PayMyBudy.service.TransferService;
+import com.PayMyBudy.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+
+public class TransferController {
+
+    private final TransferService transferService;
+    private final ConnectionService connectionService;
+    private final UserService userService;
+
+    public TransferController(TransferService transferService, ConnectionService connectionService, UserService userService) {
+        this.transferService = transferService;
+        this.connectionService = connectionService;
+        this.userService = userService;
+    }
+
+    @PostMapping("transfer")
+    public ModelAndView transfer(Model model, @ModelAttribute("transferForm") TransferForm form) {
+        transferService.transfer(form);
+        List<String> contacts = connectionService.findConnectionsEmail();
+        List<Transfer> transactions = transferService.findTransactions();
+        model.addAttribute("connections", contacts);
+        model.addAttribute("transfers", transactions);
+        return new ModelAndView("transfer","tranferForm", new TransferForm());
+    }
+
+    @GetMapping("transfer")
+    public ModelAndView transfer(Model model) {
+        List<String> contacts = connectionService.findConnectionsEmail();
+        List<Transfer> transactions = transferService.findTransactions();
+        model.addAttribute("connections",contacts);
+        model.addAttribute("transfers", transactions);
+        return new ModelAndView("transfers","transferForm", new TransferForm());
+    }
+    @GetMapping("transfer-to-bank")
+    public ModelAndView transferToBank(Model model) {
+        String iban = transferService.findIban();
+        User account = userService.findAccount();
+        model.addAttribute("account", account);
+        model.addAttribute("iban", iban);
+        return new ModelAndView("transfer-to-bank", "transferToBankForm", new TransferToBankForm());
+    }
+    @PostMapping("transfer-to-bank")
+    public ModelAndView transferCashToBank(Model model, @ModelAttribute("transferToBankForm") TransferToBankForm form) {
+        transferService.transferToBank(form);
+        String iban = transferService.findIban();
+        User account = userService.findAccount();
+        model.addAttribute("account", account);
+        model.addAttribute("iban", iban);
+        return new ModelAndView("transfer-to-bank", "transferToBankForm", new TransferToBankForm());
+    }
+    @GetMapping("transfer-to-account")
+    public ModelAndView transferToaccount(Model model) {
+        return new ModelAndView("transfer-to-account", "transferToAccountForm", new TransferToAccountForm());
+    }
+    @PostMapping("transfer-to-account")
+    public ModelAndView transferCashToAccount(Model model, @ModelAttribute())
+
+}
